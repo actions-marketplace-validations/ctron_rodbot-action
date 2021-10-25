2893f421295097cb9efd0bfd042abbd69145fada
Jens Reimann <jreimann@redhat.com>
2021-10-25 12:50:40 +0200

    Initial import

---
+++ README.md
@@ -0,0 +1,1 @@
+# rodbot Action
---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+ARCH=$(uname -m)
+DIR="$RUNNER_TOOL_CACHE/rodbot/$VERSION/$ARCH"
+: "${VERSION:=0.1.1}"
+
+mkdir -p "$DIR"
+
+if [ ! -x "$DIR/rodbot" ]; then
+    case $ARCH in
+        x86_64)
+            curl -sL "https://github.com/ctron/rodbot/releases/download/${VERSION}/rodbot-linux-amd64" -o "$DIR/rodbot"
+            chmod a+x "$DIR/rodbot"
+            ;;
+
+        *)
+            echo "Unknown architecture: $ARCH"
+            exit 1
+            ;;
+    esac
+fi
